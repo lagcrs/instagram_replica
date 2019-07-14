89fe4842cfc70082d70a1100c692bd4dfacc8629
Larissa Silva <larissa.gcrs@gmail.com>
2019-07-14 14:41:11 -0300

    create main header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter >
-      <Routes />
+    	<Header/>
+		<Routes />
     </BrowserRouter>
   );
 }
