bc96a281885fe5f734c9cecec6c00d3e97e4a2e1
Larissa Silva <larissa.gcrs@gmail.com>
2019-07-14 15:46:31 -0300

    Axios and update Feed Data

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,56 +1,50 @@
 import React, { Component } from 'react';
+import api from '../services/api'
+
 import './Feed.css';
+
 import more from '../assets/more.svg';
 import like from '../assets/like.svg';
 import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
 export default class Feed extends Component{
+    state = {
+        feed: [],
+    };
+
+    async componentDidMount() {
+        const response = await api.get('posts');
+
+        this.setState({ feed: response.data });
+    }
+
     render(){
         return(
             <section id="post-list">
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Larissa Georgina</span>
-                            <span className="place">São Paulo</span>
-                        </div>
-                        <img src={more} alt="Mais"/>
-                    </header>
-                    <img src="http://localhost:3333/files/7998_star_wars.jpg" alt=""/>
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt="Like"/>
-                            <img src={comment} alt="Comment"/>
-                            <img src={send} alt="Send"/>
-                        </div>
+                { this.state.feed.map(post => (
+                   <article>
+                        <header>
+                            <div className="user-info">
+                                <span>{ post.author }</span>
+                                <span className="place">{ post.place }</span>
+                            </div>
+                            <img src={more} alt="Mais"/>
+                        </header>
+                        <img src={`http://localhost:3333/files/${post.image}`} alt=""/>
+                        <footer>
+                            <div className="actions">
+                                <img src={like} alt="Like"/>
+                                <img src={comment} alt="Comment"/>
+                                <img src={send} alt="Send"/>
+                            </div>
 
-                        <strong>900 curtidas</strong>
-                        <p>Um post muito massa!</p>
-                        <span>#react #omnistack</span>
-                    </footer>
-                </article>
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Larissa Georgina</span>
-                            <span className="place">São Paulo</span>
-                        </div>
-                        <img src={more} alt="Mais"/>
-                    </header>
-                    <img src="http://localhost:3333/files/7998_star_wars.jpg" alt=""/>
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt="Like"/>
-                            <img src={comment} alt="Comment"/>
-                            <img src={send} alt="Send"/>
-                        </div>
-
-                        <strong>900 curtidas</strong>
-                        <p>Um post muito massa!</p>
-                        <span>#react #omnistack</span>
-                    </footer>
-                </article>
+                            <strong>{post.likes} curtidas</strong>
+                            <p>{post.description}</p>
+                            <span>{ post.hashtags }</span>
+                        </footer>
+                    </article> 
+                )) }
             </section>
         )
     }
